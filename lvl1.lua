5b4a98cd8d5a6918fc810581de477c55b41e8f65
fanyin <fanyin1234@gmail.com>
2013-09-02 15:59:30 -0400

    gird

--- lvl1.lua
+++ lvl1.lua
@@ -1,33 +1,23 @@
 local lvl1 = {}
 local GS = require "hump.gamestate"
 
+local speed = 5
+local pos = 0
+local tileSize = 50
 
-function lvl1:init()
-    --nothing
-end
+local width = 800
+local height = 600
 
-function lvl1:draw()
-    --love.graphics.printf("This is the first level. Lorem Ipsum and all that shit", 120, 200, 150, "center")
-end
-
-function lvl1:mousepressed(x, y, button)
-    --nothing yet
-end
-
-function lvl1:leave()
-    --nothing yet
-end
-
-
---Generates a grid 
+--Generates a grid
 local function genGrid(width, height)
     local newGrid = {}
-    for i = 1, width do
+    for i = 1, width/tileSize do
         newGrid[i] = {}
-        for j = 1, height do
+        for j = 1, height/tileSize do
             newGrid[i][j] = 0
         end
     end
+    return newGrid
 end
 
 --Locates the grid coordinate based on mouseX, mouseY, and tileSize
@@ -37,15 +27,45 @@ local function locateGridPos(mouseX, mouseY, tileSize)
 end
 
 --Fills in the grid given two coordinates
-local function fillGrid(grid, tileSize, x, y)
-    if(x and y) then 
-        for k, v in pairs(grid) do 
-            for i, j in pairs(v) do 
+local function fillGrid(grid, tileSize)
+    for k, v in pairs(grid) do
+        for i, j in pairs(v) do
+            if j == 1 then
                 love.graphics.rectangle("fill", tileSize*(k - 1), tileSize*(i - 1),
                 tileSize, tileSize)
             end
         end
     end
 end
+
+local grid = genGrid(width, height)
+
+
+function lvl1:update(dt)
+    pos = pos + speed * dt
+end
+
+function lvl1:init()
+    --nothing
+    love.graphics.setColor(255,0,0)
+end
+
+function lvl1:draw()
+    --love.graphics.printf("This is the first level. Lorem Ipsum and all that shit", 120, 200, 150, "center")
+    fillGrid(grid, tileSize)
+end
+
+function lvl1:mousepressed(x, y, button)
+    if button == 'l' then
+        gx, gy = locateGridPos(x,y, tileSize)
+        grid[gx][gy] = 1 - grid[gx][gy]
+    end
+end
+
+function lvl1:leave()
+    --nothing yet
+end
+
+
 
 return lvl1
