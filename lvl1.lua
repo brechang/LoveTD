99c4a792a2ff019956ced494796f064296238f0c
Kevin Lu <luhkevin@gmail.com>
2013-09-02 15:21:21 -0400

    gamestate switching added

--- lvl1.lua
+++ lvl1.lua
@@ -7,7 +7,7 @@ function lvl1:init()
 end
 
 function lvl1:draw()
-    --nothing yet
+    love.graphics.printf("This is the first level. Lorem Ipsum and all that shit", 120, 200, 150, "center")
 end
 
 function lvl1:mousepressed(x, y, button)
@@ -17,3 +17,36 @@ end
 function lvl1:leave()
     --nothing yet
 end
+
+
+--All these local bitches will be moved into some external module
+--Generates a grid 
+local function genGrid(width, height)
+    local newGrid = {}
+    for i = 1, width do
+        newGrid[i] = {}
+        for j = 1, height do
+            newGrid[i][j] = 0
+        end
+    end
+end
+
+--Locates the grid coordinate based on mouseX, mouseY, and tileSize
+local function locateGridPos(mouseX, mouseY, tileSize)
+    local gridX, gridY = math.ceil(mouseX / tileSize), math.ceil(mouseY/tileSize)
+    return gridX, gridY
+end
+
+--Fills in the grid given two coordinates
+local function fillGrid(grid, tileSize, x, y)
+    if(x and y) then 
+        for k, v in pairs(grid) do 
+            for i, j in pairs(v) do 
+                love.graphics.rectangle("fill", tileSize*(k - 1), tileSize*(i - 1),
+                tileSize, tileSize)
+            end
+        end
+    end
+end
+
+return lvl1
--- main.lua
+++ main.lua
@@ -1,23 +1,44 @@
 GS = require "hump.gamestate"
 lvl1 = require "lvl1"
 
-local mainMenu = {}
+local mainMenu = {started = false}
 
 function love.load()
-    love.graphics.setMode(600, 800, false, true)
+    love.graphics.setMode(800, 600, false, true)
     love.graphics.setColor(0, 255, 0)
 
     GS.registerEvents()
+    GS.switch(mainMenu)
 
 end
 
 function love.draw()
-    love.graphics.rectangle("line", 0, 0, 400, 400)
+    love.graphics.rectangle("line", 0, 0, 800, 600)
+end
+
+function love.keypressed(key)
+    if key == 'q' then
+        love.event.quit()
+    end
 end
 
 --Game code
 function mainMenu:draw()  
     --Stuff happens and gamestate switches
-    GS.switch(lvl1)
+    if mainMenu.started then
+        GS.switch(lvl1)
+    end
+
+    love.graphics.printf("Welcome to LoveTD. Press 'q' to quit and 's' to start",
+    120, 200, 150, "center")
+end
+
+function mainMenu:init()
+    mainMenu.started = false
 end
     
+function mainMenu:keyreleased(key)
+    if key == 's' then
+        mainMenu.started = true
+    end
+end    
