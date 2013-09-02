e960196760f84f45c7fdda4a7b64c0903865b1de
brechang <brechang@seas.upenn.edu>
2013-09-02 16:30:43 -0400

    added radius

--- tower.lua
+++ tower.lua
@@ -2,7 +2,8 @@ Class = require 'hump.class'
 
 Tower = Class{}
 
-function Tower:init(health, attack)
+function Tower:init(health, attack, radius)
 	self.health = health
 	self.attack = attack
+	self.radius = radius
 end
