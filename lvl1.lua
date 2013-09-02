8da31e5b4756c64b68981d32e6b96ffadfc11de2
Kevin Lu <luhkevin@gmail.com>
2013-09-02 14:01:02 -0400

    added functions to lvl1.lua

---
+++ lvl1.lua
@@ -0,0 +1,19 @@
+local lvl1 = {}
+local GS = require "hump.gamestate"
+
+
+function lvl1:init()
+    --nothing
+end
+
+function lvl1:draw()
+    --nothing yet
+end
+
+function lvl1:mousepressed(x, y, button)
+    --nothing yet
+end
+
+function lvl1:leave()
+    --nothing yet
+end
