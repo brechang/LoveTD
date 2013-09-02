2ef7b48b133d8a74665d9090347e646dbdf75013
brechang <brechang@seas.upenn.edu>
2013-09-02 16:36:23 -0400

    added setters and getters

--- enemy.lua
+++ enemy.lua
@@ -8,3 +8,19 @@ function Enemy:init(health, attack, speed, pos)
 	self.speed = speed
 	self.pos = pos
 end
+
+function Enemy:getHealth()
+	return self.health
+end
+
+function Enemy:getAttack()
+	return self.attack
+end
+
+function Enemy:getSpeed()
+	return self.speed
+end
+
+function Enemy:setHealth(hp)
+	self.health = hp
+end
