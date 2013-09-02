0e5778d65e550fe9c575ff0d0be94891d149e9be
brechang <brechang@seas.upenn.edu>
2013-09-02 14:42:18 -0400

    oops forgot position

--- enemy.lua
+++ enemy.lua
@@ -2,8 +2,9 @@ Class = require 'hump.class'
 
 Enemy = Class{}
 
-function Enemy:init(health, attack, speed)
+function Enemy:init(health, attack, speed, pos)
 	self.health = health
 	self.attack = attack
 	self.speed = speed
+	self.pos = pos
 end
